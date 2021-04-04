55214ee04ca3a32be18050b3f29051acf41812ed
aliperbol <alicititat@gmail.com>
2021-04-04 16:47:29 +0000

    casos tabla azul #18

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json());
 
 app.use("/", express.static(path.join(__dirname,"public")));
 
+
+
+
+
 //foodConsumption-stats
 app.get("/info/foodconsumption-stats", (req, res) => {
     res.send("<html><body><table border='1'><tr><th>country</th> <th>year</th> <th>food-type</th> <th>calory-per-person</th> <th>gram-per-person</th> <th>daily-gram</th> <th>daily-calory</th></tr> <tr> <td>China</td> <td>2011</td> <td>Meat</td> <td>509</td> <td>254</td> <td>2368</td> <td>3073</td> </tr> <tr> <td>United States</td> <td>2007</td> <td>Grain</td> <td>828</td> <td>303</td> <td>2877</td> <td>3771</td> </tr> <tr> <td>Spain</td> <td>2008</td> <td>Sugar & Fat</td> <td>955</td> <td>162</td> <td>2559</td> <td>3192</td> </tr> <tr> <td>Germany</td> <td>2010</td> <td>Produce</td> <td>279</td> <td>625</td> <td>2594</td> <td>3501</td> </tr> <tr> <td>India</td> <td>2009</td> <td>Meat</td> <td>28</td> <td>28</td> <td>1250</td> <td>2409</td> </tr></table></body></html>");
@@ -25,6 +29,70 @@ app.get("/info/foodconsumption-stats", (req, res) => {
 app.post("/info/foodconsumption-stats", (req, res) => {
      res.send("<html><body><table class='default'><tr><th>country</th> <th>year</th> <th>food-type</th> <th>calory-per-person</th> <th>gram-per-person</th> <th>daily-gram</th> <th>daily-calory</th></tr> <tr> <td>China</td> <td>2011</td> <td>Meat</td> <td>509</td> <td>254</td> <td>2368</td> <td>3073</td> </tr> <tr> <td>United States</td> <td>2007</td> <td>Grain</td> <td>828</td> <td>303</td> <td>2877</td> <td>3771</td> </tr> <tr> <td>Spain</td> <td>2008</td> <td>Sugar & Fat</td> <td>955</td> <td>162</td> <td>2559</td> <td>3192</td> </tr> <tr> <td>Germany</td> <td>2010</td> <td>Produce</td> <td>279</td> <td>625</td> <td>2594</td> <td>3501</td> </tr> <tr> <td>India</td> <td>2009</td> <td>Meat</td> <td>28</td> <td>28</td> <td>1250</td> <td>2409</td> </tr></table></body></html>");
 });
+
+var foodconsumption = [];
+var foodconsumptionInitialData = [
+	{
+		"country": "China",
+		"year": 2011,
+		"foodtype": "Meat",
+		"caloryperperson": 509,
+		"gramperperson": 254,
+		"dailygram": 2368,
+		"dailycalory": 3070
+	},
+	{
+		"country": "United States",
+		"year": 2007,
+		"foodtype": "Grain",
+		"caloryperperson": 828,
+		"gramperperson": 303,
+		"dailygram": 2877,
+		"dailycalory": 3771
+	}
+];
+
+app.get(BASE_API_PATH+"/foodconsumption-stats", (req,res)=>{
+	res.send(JSON.stringify(foodconsumption,null,2));
+});
+
+app.get(BASE_API_PATH+"/foodconsumption-stats/loadInitialData", (req, res)=>{
+	for(var i=0; i<foodconsumptionInitialData.length; i++){
+		foodconsumption.push(foodconsumptionInitialData[i]);
+		console.log(foodconsumptionInitialData[i].country);
+	}
+	
+	res.send("Datos cargados");
+});
+
+app.get(BASE_API_PATH+"/foodconsumption-stats/China", (req, res)=>{
+	for(var i=0; i<foodconsumptionInitialData.length; i++){
+		if(foodconsumption[i].country=="China"){
+			res.send(JSON.stringify(foodconsumptionInitialData[i],null,2));
+			
+		}
+	}
+});
+app.get(BASE_API_PATH+"/foodconsumption-stats/China/2011", (req, res)=>{
+	for(var i=0; i<foodconsumptionInitialData.length; i++){
+		if(foodconsumption[i].country=="China"&&foodconsumption[i].year==2011){
+			res.send(JSON.stringify(foodconsumptionInitialData[i],null,2));
+			
+		}
+	}
+});
+
+app.post(BASE_API_PATH+"/foodconsumption-stats", (req,res)=>{
+	var newFoodconsumption =req.body;
+	console.log(`Nuevo objeto en Obesity: <${JSON.stringify(newFoodconsumption,null,2)}>`);
+	foodconsumption.push(newFoodconsumption);
+	
+	res.sendStatus(201);
+});
+
+
+
+
 
 
 //sanity-stats
@@ -35,6 +103,11 @@ app.get("/info/sanity-stats", (req, res) => {
 app.post("/info/sanity-stats", (req, res) => {
     res.send("<html><body><table border='1'><tr><th>country</th> <th>year</th> <th>health-expenditure-in-percentage</th> <th>doctor-per-1000-habitant</th> <th>hospital-bed</th></tr> <tr><td>China</td> <td>2011</td> <td>10'61685285</td> <td>1'5</td> <td>3705100</td> </tr> <tr> <td>United States</td> <td>2007</td> <td>19'01326713</td> <td>2'41</td> <td>945199</td>  </tr> <tr> <td>Spain</td> <td>2008</td> <td>15'73140496</td> <td>4'67</td> <td>146934</td></tr> <tr> <td>Germany</td> <td>2010</td> <td>18'14760105</td> <td>3'775</td> <td>674473</td> </tr> <tr> <td>India</td> <td>2009</td> <td>4'36884071</td> <td>0'264</td> <td>540328</td> </tr></table></body></html>");
 });
+
+
+
+
+
 //obesity-stats
 app.get("/info/obesity-stats", (req, res) => {
 res.send("<html><body><table border='1'><tr><th>country</th> <th>year</th> <th>man-percent</th> <th>woman-percent</th> <th>total-population</th></tr> <tr><td>China</td> <td>2011</td> <td>4.2</td> <td>5.2</td> //<td>1376498048</td> </tr> <tr> <td>United States</td> <td>2007</td> <td>29.1</td> <td>31.5</td> <td>300608000</td>  </tr> <tr> <td>Spain</td> <td>2008</td> <td>20.8</td> <td>21</td> <td>46069000</td></tr> <tr> //<td>Germany</td> <td>2010</td> <td>21.1</td> <td>18.6</td> <td>80827000</td> </tr> <tr> <td>India</td> <td>2009</td> <td>1.7</td> <td>3.6</td> <td>1217725952</td> </tr></table></body></html>");
@@ -43,6 +116,7 @@ res.send("<html><body><table border='1'><tr><th>country</th> <th>year</th> <th>m
 app.post("/info/obesity-stats", (req, res) => {
 res.send("<html><body><table border='1'><tr><th>country</th> <th>year</th> <th>man-percent</th> <th>woman-percent</th> <th>total-population</th></tr> <tr><td>China</td> <td>2011</td> <td>4.2</td> <td>5.2</td> <td>1376498048</td> </tr> <tr> <td>United States</td> <td>2007</td> <td>29.1</td> <td>31.5</td> <td>300608000</td>  </tr> <tr> <td>Spain</td> <td>2008</td> <td>20.8</td> <td>21</td> <td>46069000</td></tr> <tr> <td>Germany</td> <td>2010</td> <td>21.1</td> <td>18.6</td> <td>80827000</td> </tr> <tr> <td>India</td> <td>2009</td> <td>1.7</td> <td>3.6</td> <td>1217725952</td> </tr></table></body></html>");
 });
+
 var obesity = [];
 var obesityInitialData = [
 	{
@@ -89,21 +163,6 @@ app.post(BASE_API_PATH+"/obesity-stats", (req,res)=>{
 	res.sendStatus(201);
 });
 
-var china= [{
-		"country": "China",
-		"year": 2011,
-		"man-percent": 4.2,
-		"woman-percent": 5.2,
-		"total-population": 1376498048
-	}];
-
-app.get(BASE_API_PATH+"/obesity-stats/china", (req,res)=>{
-	res.send(JSON.stringify(china,null,2));
-});
-
-app.delete(BASE_API_PATH+"/obesity-stats/china", (req,res)=>{
-	res.send(JSON.stringify(china,null,2));
-});
 
 
 app.get("/cool",(request,response) => {
