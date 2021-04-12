fd1798fca2c4eacd0758e9d2bdd6bbec957c69b1
aliperbol <alicititat@gmail.com>
2021-04-12 16:01:17 +0000

    public subido

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ var PORT = (process.env.PORT || 1607);
 
 var app = express();
 app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname,"public")));
+
+
 
 food_consumptionAPI.register(app);
 sanityAPI.register(app);
