dd23db9be2220ad0c0c45ee47225eaa52ea4092e
aliperbol <alicititat@gmail.com>
2021-05-03 19:48:25 +0000

    landing estilizado

--- src/frontend/Header.svelte
+++ src/frontend/Header.svelte
@@ -16,7 +16,9 @@
   
 </script>
 <main>
-<Navbar style="background-color:#24355C;" dark class="fixed-top">
+
+<Navbar style="background-color:#24355C;" dark
+class="fixed-top">
   <NavbarBrand href="/" class="mr-auto"><strong>SOS2021-10</strong></NavbarBrand>
   <NavbarToggler on:click={toggleNav} className="mr-2" />
   <Collapse {isOpen} navbar>
