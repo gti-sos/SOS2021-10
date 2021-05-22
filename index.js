b73ac9e1e0463f6cd16f791d0168152ff4f30097
almgonlop <almu.gon.12@gmail.com>
2021-05-22 12:38:55 +0000

    obesity

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ var path = require("path");
 
 var bodyParser = require("body-parser");
 
-var food_consumptionAPIv1 =  require("./src/backend/v1/foodconsumptionAPI");
-var food_consumptionAPIv2 =  require("./src/backend/v2/foodconsumptionAPI");
-var sanityAPIv1 =  require("./src/backend/v1/sanityAPI");
-var sanityAPIv2 =  require("./src/backend/v2/sanityAPI");
-var obesityAPIv1 =  require("./src/backend/v1/obesityAPI");
+
 var obesityAPIv2 =  require("./src/backend/v2/obesityAPI");
 
 var PORT = (process.env.PORT || 10000);
@@ -20,11 +16,7 @@ app.use(bodyParser.json());
 app.use("/", express.static(path.join(__dirname,"public")));
 
 
-food_consumptionAPIv2.register(app);
-food_consumptionAPIv1.register(app);
-sanityAPIv1.register(app);
-sanityAPIv2.register(app);
-obesityAPIv1.register(app);
+
 obesityAPIv2.register(app);
 
 
--- src/frontend/NotFound.svelte
+++ src/frontend/NotFound.svelte
@@ -1,10 +1,10 @@
 <script>
 
-	import Header from './Header.svelte';
+	
 </script>
 
 <main>
-	<Header/>
+	
 	<div style="margin:auto; width:60%; text-align: center; padding-top: 275px;">
 		<br>
 		<h3>Este recurso no existe</h3>
