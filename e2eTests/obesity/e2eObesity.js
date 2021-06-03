6ec9c6f7e39dce1bdc58bb1e5150a4406abdd9ce
almgonlop <almu.gon.12@gmail.com>
2021-06-03 17:55:35 +0200

    obesity #39

--- e2eTests/obesity/e2eObesity.js
+++ e2eTests/obesity/e2eObesity.js
@@ -1,10 +1,21 @@
 const puppeteer = require('puppeteer');
 
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.informatica.us.es');
-  await page.screenshot({ path: 'e2eTests/example.png' });
+  await page.goto('http://sos2021-10.herokuapp.com/#/obesity-stats');
+  await page.screenshot({ path: 'e2eTests/capturas/info.png' });
+
+  /*const [response]= await Promise.all([
+    page.waitForNavigation(),
+    page.click("body > main > main > div > div > div > h4 > a"),
+  ]);
+  await page.screenshot({ path: 'e2eTests/capturas/home.png' });*/
+  
+  await page.click("body > main > main > button.btn.btn-outline-warning");
+  await page.waitForTimeout(1000);
+  await page.screenshot({ path: 'e2eTests/capturas/obesity.png' });
 
   await browser.close();
 })();
