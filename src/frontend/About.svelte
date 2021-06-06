f071e9b5b6eb23d9a09719896c96d1526e9f5bde
aliperbol <alicititat@gmail.com>
2021-06-06 18:00:39 +0200

    about page

--- src/frontend/About.svelte
+++ src/frontend/About.svelte
@@ -12,7 +12,40 @@ import {
     <h2>
 		Vídeos de presentación:
 	</h2>
-	<ul>
+
+	<div class="card-deck">
+        <div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+              <iframe width="816" height="441" src="https://youtu.be/3ZNAJTGD59Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Vídeo API Sanidad</h5>
+            <p class="card-text"></p>
+          </div>
+        </div>
+
+        <!--<div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+                <iframe width="900" height="506" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Vídeo API Obesidad</h5>
+            <p class="card-text"></p>
+          </div>
+        </div> -->
+
+        <div class="card">
+            <div class="embed-responsive embed-responsive-16by9 video">
+              <iframe width="757" height="409" src="https://youtu.be/YRzkvq7f7vA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          <div class="card-body">
+            <h5 class="card-title">Vídeo API Consumo de Comida</h5>
+            <p class="card-text"></p>
+          </div>
+        </div>
+    </div>
+
+	<!-- <ul>
 		<Breadcrumb class="peque">
   			<BreadcrumbItem active>Vídeo API Sanidad</BreadcrumbItem>
   			<BreadcrumbItem><a href="https://youtu.be/3ZNAJTGD59Y">Enlace a youtube</a></BreadcrumbItem>
@@ -29,6 +62,6 @@ import {
 			
 			</Breadcrumb>
 		
-	</ul>
+	</ul> -->
 
 </main>
