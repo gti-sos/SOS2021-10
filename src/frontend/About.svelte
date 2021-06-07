458a69f825276e0a69c9327a64e99374093281ac
aliperbol <alicititat@gmail.com>
2021-06-07 18:07:44 +0200

    D03

--- src/frontend/About.svelte
+++ src/frontend/About.svelte
@@ -16,7 +16,7 @@ import {
 	<div class="card-deck">
         <div class="card">
             <div class="embed-responsive embed-responsive-16by9 video">
-              <iframe width="816" height="441" src="https://youtu.be/3ZNAJTGD59Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="816" height="441" src="https://www.youtube.com/embed/3ZNAJTGD59Y" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           <div class="card-body">
             <h5 class="card-title">Vídeo API Sanidad</h5>
@@ -24,19 +24,19 @@ import {
           </div>
         </div>
 
-        <!--<div class="card">
+        <div class="card">
             <div class="embed-responsive embed-responsive-16by9 video">
-                <iframe width="900" height="506" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="900" height="506" src="https://www.youtube.com/embed/ZsRnlI9mp2A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           <div class="card-body">
             <h5 class="card-title">Vídeo API Obesidad</h5>
             <p class="card-text"></p>
           </div>
-        </div> -->
+        </div>
 
         <div class="card">
             <div class="embed-responsive embed-responsive-16by9 video">
-              <iframe width="757" height="409" src="https://youtu.be/YRzkvq7f7vA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe width="757" height="409" src="https://www.youtube.com/embed/YRzkvq7f7vA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
           <div class="card-body">
             <h5 class="card-title">Vídeo API Consumo de Comida</h5>
