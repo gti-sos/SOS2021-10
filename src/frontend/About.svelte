2e5abda865f8111286d5b0147152fd37d0d91c92
aliperbol <alicititat@gmail.com>
2021-06-07 18:13:31 +0200

    D03

--- src/frontend/About.svelte
+++ src/frontend/About.svelte
@@ -45,23 +45,5 @@ import {
         </div>
     </div>
 
-	<!-- <ul>
-		<Breadcrumb class="peque">
-  			<BreadcrumbItem active>Vídeo API Sanidad</BreadcrumbItem>
-  			<BreadcrumbItem><a href="https://youtu.be/3ZNAJTGD59Y">Enlace a youtube</a></BreadcrumbItem>
-			
-			
-		</Breadcrumb>
-		<Breadcrumb>
-  			<BreadcrumbItem active>Vídeo API Obesidad </BreadcrumbItem>
-  			<BreadcrumbItem><a href="https://documenter.getpostman.com/view/14950492/TzRShoHF">Enlace a youtube</a></BreadcrumbItem>
-		</Breadcrumb>
-		<Breadcrumb>
-  			<BreadcrumbItem active>Vídeo API Consumo de Comida </BreadcrumbItem>
-  			<BreadcrumbItem><a href="https://documenter.getpostman.com/view/14948248/TzJoDfvx">Enlace a youtube</a></BreadcrumbItem>
-			
-			</Breadcrumb>
-		
-	</ul> -->
 
 </main>
